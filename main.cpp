5197329fb4e616525257d8401506a81de7345655
Chien Tran <info.chientran@gmail.com>
2024-02-29 10:06:12 +0700

    #3

--- main.cpp
+++ main.cpp
@@ -2,54 +2,32 @@
 using namespace std;
 
 /*
- * input: nhập dữ liệu từ bàn phím 3 số tự nhiên a,b,c != 0
- * điều kiện: b<a<c, a chia hết cho 2
- * yêu cầu:
- * - viết hàm tính tổng của a,b,c
- * - tính phần dư của a/b,
- * - tính tổng của phần dư a/b + c/b
+ * [Lesson-20] Valid Parentheses
+ *
+ * _____EN_____
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * 1. Open brackets must be closed by the same type of brackets
+ * 2. Open brackets must be closed in the correct order
+ * 3. Every close bracket has a corresponding open bracket of the same type
+ *
+ * Constraints:
+ * 1. 1 <= s.length <= 104
+ * 2. s consists of parentheses only '()[]{}'.
+ *
+ *
+ * _____VN_____
+ * Cho một chuỗi s trong đó chứa các kí tự (, ), {, }, [ và ]. Hãy kiểm tra xem chuỗi s có phải chuỗi hợp lệ với các tiêu chí sau:
+ * 1. Dấu ngoặc mở phải được đóng bằng dấu ngoặc cùng loại.
+ * 2. Dấu ngoặc mở phải được đóng theo đúng thứ tự.
+ * 3. Mỗi dấu ngoặc đóng có một dấu ngoặc mở tương ứng cùng loại.
+ *
+ * Điều kiện
+ * 1. 1 <= s.length
+ *
  */
 
-// Function
-int sum(int a, int b, int c) {
-    return a + b + c;
-}
-
-int calculateRemainderAB (int a, int b) {
-    return b % a;
-}
-
-int calculateRemainderABC (int a, int b, int c) {
-    return (b % a) + (c % b);
-}
-
-int main() {
-    int firstNumber, secondNumber, thirdNumber;
-
-    // Input
-
-    cout << "Enter first number: " << endl;
-    cin >> firstNumber;
-
-    if (firstNumber < 1) {
-        return 0;
-    }
-
-    cout << "Enter second number: " << endl;
-    cin >> secondNumber;
-
-    if (firstNumber > secondNumber || secondNumber < 1 || secondNumber % 2 != 0) {
-        return 0;
-    }
-
-    cout << "Enter third number: " << endl;
-    cin >> thirdNumber;
-
-    if (thirdNumber < secondNumber || thirdNumber < firstNumber || thirdNumber < 1) {
-        return 0;
-    }
-
-    cout << "Sum = " << sum(firstNumber, secondNumber, thirdNumber) << endl;
-    cout << "calculate remainder a/b = " << calculateRemainderAB(firstNumber, secondNumber) << endl;
-    cout << "calculate remainder a/b + c/b = " << calculateRemainderABC(firstNumber, secondNumber, thirdNumber);
-}
+class Solution {
+public:
+    bool isValid(string s) {}
+};
