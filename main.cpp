0d4ce466e3b68fa63cd2dee2aa6b14f08bc060a5
Chien Tran <info.chientran@gmail.com>
2024-02-29 10:37:58 +0700

    update gitignore

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
- * [Lession] Two Sum
+ * [Lession-1] Two Sum
  *
  * _____EN_____
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
