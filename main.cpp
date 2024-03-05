89cfdc19385465f1047e7fcb15d69539a7edd568
Chien Tran <info.chientran@gmail.com>
2024-03-05 10:32:07 +0700

    ex4

--- main.cpp
+++ main.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void getMinMax(int arr[], int arrLength) {
-    int min = arr[0];
-    int max = arr[0];
-
-    for (int i = 0; i < arrLength; ++i) {
-        if (arr[i] < min) min = arr[i];
-        if (arr[i] > max) max = arr[i];
-    }
-
-    cout << min << endl;
-    cout << max << endl;
+void Swap (int &x, int &y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-void Swap(int *x, int *y) {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
+void ArrangeArr ()
+{
+    int myArr[5] = {3, 5, 2, 4, 1};
+    int myArrLength = sizeof(myArr) / sizeof(int);
 
-void arrangeArr(int arr[], int arrLength) {
-    for (int i = 0; i < arrLength; ++i) {
-        int min = i;
+    for (int i = 0; i < myArrLength - 1; ++i) {
+        for (int j = i + 1; j < myArrLength; ++j) {
 
-        for (int j = i + 1; j < arrLength; ++j) {
-            if (arr[j] < arr[min]) min = j;
+            if (myArr[i] > myArr[j]) {
+                Swap(myArr[i], myArr[j]); //&myArr[i] trả về địa chỉ của biến đó
+            }
         }
-        Swap(&arr[i], &arr[min]);
     }
 
-    for(int i = 0; i < arrLength; i++){
-        cout << arr[i] << " ";
+    for (int i = 0; i < myArrLength; ++i) {
+        cout << myArr[i] << " ";
     }
+
 }
 
-int main() {
-    int arr[5] = {14, 54, 2, 1, 5};
-    int arrLength = sizeof(arr) / sizeof(int);
 
-    getMinMax(arr, arrLength);
-    arrangeArr(arr, arrLength);
+
+int main()
+{
+    ArrangeArr();
 
     return 0;
 }
