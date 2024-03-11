2504237c434b07473c6cac4f12215b68ccbbddc3
Chien Tran <info.chientran@gmail.com>
2024-03-11 13:34:41 +0700

    bubble-sort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-/*
- * [Sort]
- *
- */
-
 #include <iostream>
 using namespace std;
+
+void getArr(int arr[], int length) {
+    for (int i = 0; i < length; ++i) {
+        cout << "Enter arr [" << i << "]:" << endl;
+        cin >> arr[i];
+    }
+}
 
 void Swap(int &x,int &y) {
     int temp = x;
@@ -12,29 +14,41 @@ void Swap(int &x,int &y) {
     y = temp;
 }
 
-void prinArr(int arr[], int arrLength) {
-    for(int i = 0; i < arrLength; i++) {
+void prinArr(int arr[], int length) {
+    for(int i = 0; i < length; i++) {
         cout << arr[i] << " ";
     }
 }
 
-void bubbleSort(int arr[], int arrLength) {
-    for(int i = 0; i < arrLength; i++) {
-        for(int j = 0; j < arrLength -1; j++) {
-            if(arr[j] > arr[j+1]) {
-                Swap(arr[i], arr[j]);
+
+void bubbleSort(int arr[], int length) {
+    for(int i = 0; i < length; i++) {
+        for(int j = 0; j < length -1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                Swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
+
+
 int main() {
-    int arr[5] = {4, 8, 5, 7, 9};
-    int arrLength = sizeof(arr) / sizeof(int);
+    int length;
+    cout << "Enter array length: " << endl;
+    cin >> length;
 
+    int *arr = new int[length];
 
-    bubbleSort(arr, arrLength);
-    prinArr(arr, arrLength);
+    getArr(arr, length);
+
+    bubbleSort(arr, length);
+
+    for(int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
 
     return 0;
 }
