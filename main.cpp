a459d6b68e74969d956222b3f070aa513793659a
Chien Tran <info.chientran@gmail.com>
2024-03-14 13:58:43 +0700

    array/ex-1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 /*
- * [Lession]
  * [Exercise]
  *
+ * Viết một chương trình c++ để thêm vào các phần tử trong một mảng và hiển thị chúng
+ *
  */
+
+#include <iostream>
+using namespace std;
+
+void getArr(int arr[], int length) {
+    for (int i = 0; i < length; ++i) {
+        cout << "Enter array[" << i << "]: " << endl;
+        cin >> arr[i];
+    }
+}
+
+void displayArr(int arr[], int length) {
+    cout << "Array items: " << endl;
+    for (int i = 0; i < length; ++i) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int length;
+    cout << "Enter array length: " << endl;
+    cin >> length;
+
+    int *arr = new int [length];
+    getArr(arr, length);
+    displayArr(arr, length);
+
+
+    return 0;
+}
